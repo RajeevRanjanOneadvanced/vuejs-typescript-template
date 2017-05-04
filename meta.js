889310a89f5b3d00b0a87a32c5647caa7c38db29
Martin Simard <MASIM144@ulaval.ca>
2017-05-04 15:37:37 -0400

    Convert to vue-cli template

---
+++ meta.js
@@ -0,0 +1,44 @@
+module.exports = {
+    "helpers": {
+        "if_or": function (v1, v2, options) {
+            if (v1 || v2) {
+                return options.fn(this);
+            }
+            return options.inverse(this);
+        }
+    },
+    "prompts": {
+        "name": {
+            "type": "string",
+            "required": true,
+            "label": "Project name"
+        },
+        "description": {
+            "type": "string",
+            "required": false,
+            "label": "Project description",
+            "default": "A Vue.js project"
+        },
+        "author": {
+            "type": "string",
+            "label": "Author"
+        },
+        "private": {
+            "type": "boolean",
+            "default": true
+        },
+        "router": {
+            "type": "confirm",
+            "message": "Install vue-router?"
+        },
+        "unit": {
+            "type": "confirm",
+            "message": "Setup unit tests with Karma + Jasmine?"
+        },
+        "tslint": {
+            "type": "confirm",
+            "message": "Setup lint with TSLint?"
+        }
+    },
+    "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev"
+}
