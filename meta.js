a4d4000345f03487aeccc99db52479791f56ef79
Martin Simard <martin.simard@dti.ulaval.ca>
2017-06-08 15:13:57 -0400

    Option to create a simple template

--- meta.js
+++ meta.js
@@ -23,39 +23,52 @@ module.exports = {
             "type": "string",
             "label": "Author"
         },
+        "webpack": {
+            "type": "confirm",
+            "message": "Install webpack dev environment? (if 'no', a simple template is installed)"
+        },
         "port": {
+            "when": "webpack",
             "type": "number",
             "label": "Port number",
             "default": 5000
         },
         "tslint": {
+            "when": "webpack",
             "type": "confirm",
             "message": "Setup lint with TSLint?"
         },
         "stylelint": {
+            "when": "webpack",
             "type": "confirm",
             "message": "Setup CSS/SASS lint with stylelint?"
         },
         "unit": {
+            "when": "webpack",
             "type": "confirm",
             "message": "Setup unit tests with Karma + Jasmine?"
         },
         "e2e": {
+            "when": "webpack",
             "type": "confirm",
             "message": "Setup e2e tests with Nightwatch + Selenium?"
         }
     },
     "filters": {
-        "conf/tslint.json": "tslint",
-        "conf/stylelint.json": "stylelint",
-        "conf/karma.conf.js": "unit",
-        "tests/unit/**/*": "unit",
-        "conf/nightwatch.conf.js": "e2e",
-        "tests/e2e/**/*": "e2e"
+        ".vscode/launch.json": "webpack",
+        "conf/**/*": "webpack",
+        "src/**/*": "webpack",
+        "conf/tslint.json": "webpack && tslint",
+        "conf/stylelint.json": "webpack && stylelint",
+        "conf/karma.conf.js": "webpack && unit",
+        "tests/**/*": "webpack && (unit || e2e)",
+        "tests/unit.js": "webpack && unit",
+        "conf/nightwatch.conf.js": "webpack && e2e",
+        "tests/e2e/**/*": "webpack && e2e"
     },
     "skipInterpolation": [
         "src/**/*",
         "test/**/*"
     ],
-    "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev"
+    "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev (or npm run tsc)"
 }
